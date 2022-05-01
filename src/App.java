8bce2f6ceb431f0c0f4878aee3d843e6520a0352
Gualtieri Agustin Manuel <gualtieriagustin@outlook.com>
2022-05-01 16:06:09 -0300

    Base test 1

---
+++ src/App.java
@@ -0,0 +1,11 @@
+
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Tornillo tornillo1 = new Tornillo("Agustin");
+
+        System.out.println(tornillo1.name());
+    }
+}
---
+++ src/Tornillo.java
@@ -0,0 +1,12 @@
+
+
+public class Tornillo {
+    public Tornillo(String name){
+        this.name=name;
+    }
+    private String name;
+
+    public String name(){
+        return name;
+    }
+}
