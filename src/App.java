e4ce4b57eb1f956e230e7c0c504c70e852fb68e9
Gualtieri Agustin Manuel <gualtieriagustin@outlook.com>
2022-05-01 20:23:11 -0300

    1.0.0

--- src/App.java
+++ src/App.java
@@ -4,8 +4,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
-        Tornillo tornillo1 = new Tornillo("Agustin");
+        Tornillo tornillo1 = new Tornillo();
 
-        System.out.println(tornillo1.name());
+        tornillo1.setMedida("A4x25");
+
+        System.out.println(tornillo1.getMedida());
+        System.out.println(tornillo1.getCodigoDelTornillo());
+
+        Tornillo tornillo2 = new Tornillo();
+
+        tornillo2.setMedida("M5x25");
+
+        System.out.println(tornillo2.getMedida());
+        System.out.println(tornillo2.getCodigoDelTornillo());
     }
 }
--- src/Tornillo.java
+++ src/Tornillo.java
@@ -1,14 +1,62 @@
 
 
 public class Tornillo {
-    public Tornillo(String name){
-        this.name=name;
+    public Tornillo(){
+       codigoDelTornillo=codigo++;
     }
     
 
-    public String name(){
+    public int getCodigoDelTornillo() {
+        return codigoDelTornillo;
+    }
+
+
+
+
+    public String getName() {
         return name;
     }
 
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getTipoDeTornillo() {
+        return TipoDeTornillo;
+    }
+
+
+    public void setTipoDeTornillo(String tipoDeTornillo) {
+        TipoDeTornillo = tipoDeTornillo;
+    }
+
+
+    public String getMedida() {
+        return Medida;
+    }
+
+
+    public void setMedida(String medida) {
+        Medida = medida;
+    }
+
+
+    public String getDenomincion() {
+        return Denomincion;
+    }
+
+
+    public void setDenomincion(String denomincion) {
+        Denomincion = denomincion;
+    }
+
+    private String Medida;
+    private String Denomincion;
     private String name;
+    private String TipoDeTornillo;
+    static int codigo = 0;
+    private int codigoDelTornillo;
+
 }
